03c59a801b373cc686b82c94cfc869e69c00896e
jaehyeonjung0613 <jaehyeonjung0613@gmail.com>
2024-12-11 15:59:26 +0900

    feat: 1. TMDB API 요청하기 - [server/apis/movie.js] 영화목록 api 작성 - [server/routes/index.js] 영화목록 JSON 문자열로 반환 - [src/constant.js] TMDB API 항목 정의 - [views/index.html] assets uri와 url 맞춤

---
+++ ssr/server/apis/movie.js
@@ -0,0 +1,7 @@
+import { FETCH_OPTIONS, TMDB_MOVIE_LISTS } from "../../src/constant.js";
+
+export const fetchMovies = async () => {
+  const response = await fetch(TMDB_MOVIE_LISTS.POPULAR, FETCH_OPTIONS);
+
+  return await response.json();
+};
--- ssr/server/routes/index.js
+++ ssr/server/routes/index.js
@@ -2,18 +2,27 @@ import { Router } from "express";
 import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
+import { fetchMovies } from "../apis/movie.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const router = Router();
 
-router.get("/", (_, res) => {
+router.get("/", async (_, res) => {
   const templatePath = path.join(__dirname, "../../views", "index.html");
-  const moviesHTML = "<p>들어갈 본문 작성</p>";
+  const moviesData = await fetchMovies();
+  const moviesHTML = `
+    <div>
+      ${JSON.stringify(moviesData)}
+    </div>
+  `;
 
   const template = fs.readFileSync(templatePath, "utf-8");
-  const renderedHTML = template.replace("<!--${MOVIE_ITEMS_PLACEHOLDER}-->", moviesHTML);
+  const renderedHTML = template.replace(
+    "<!--${MOVIE_ITEMS_PLACEHOLDER}-->",
+    moviesHTML
+  );
 
   res.send(renderedHTML);
 });
